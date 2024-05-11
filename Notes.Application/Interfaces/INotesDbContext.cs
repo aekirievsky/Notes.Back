757040b292c6680ca34e42c49510971778af5bcd
aekirievsky <aekirievsky@gmail.com>
2024-05-12 00:00:16 +0500

    Добавьте файлы проекта.

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Core.Domain;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
