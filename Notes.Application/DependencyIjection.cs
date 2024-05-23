6a6ef967dbc861c9930ccf36b3f392a6667d3aca
aekirievsky <aekirievsky@gmail.com>
2024-05-23 22:31:50 +0500

    Добавил зависимости

---
+++ Notes.Application/DependencyIjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+using System.Reflection;
+
+namespace Notes.Application
+{
+    public static class DependencyIjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
